d55e5025da1cc8d072a03303372b31e94a99cab3
CAWilson94 <charlotteawilson.94@gmail.com>
2024-04-01 16:57:34 +0100

    cors, server port and autowired added to controller

--- src/main/java/com/example/worklog/WorkItemLogController.java
+++ src/main/java/com/example/worklog/WorkItemLogController.java
@@ -1,11 +1,16 @@
 package com.example.worklog;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class WorkItemLogController {
+
+    @Autowired
+    private WorkItemLog workItemLog;
+
     @QueryMapping
     public WorkItemLog WorkItemLogById(@Argument String id){
         return WorkItemLog.getById(id);
